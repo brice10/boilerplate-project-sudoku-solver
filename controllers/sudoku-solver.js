b2a794534dd2b8aaf4443fd11242ec6599786dae
Arléon <arleonzemtsop@gmail.com>
2024-02-10 23:01:47 +0100

    Units tests and method to validate puzzle input string

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,25 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      return {
+        ok: false,
+        error: 'Expected puzzle to be 81 characters long'
+      };
+    }
+
+    const validCharacters = /^[1-9.]+$/;
+    if (!validCharacters.test(puzzleString)) {
+      return {
+        ok: false,
+        error: 'Invalid characters in puzzle'
+      };
+    }
+
+    return {
+      ok: true,
+      error: null
+    };
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -16,7 +35,7 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,54 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const { invalidPuzzleStrings, validPuzzleStrings } = require('./data/tests-data.js');
+let solver = new Solver();
 
 suite('Unit Tests', () => {
+
+    suite('Validate the given input string', function () {
+
+        test('Validate method should correctly read valid puzzle input strings', function () {
+            const puzzleStrings = validPuzzleStrings;
+            const ok = puzzleStrings.every(str => (solver.validate(str).ok));
+            const error = puzzleStrings.every(str => (solver.validate(str).error === null));
+            assert.isTrue(ok, 'Validate function should return { ok: true, ... } when puzzle input string is valid');
+            assert.isTrue(error, 'Validate function should return { error: null, ... } when puzzle input string is valid');
+        });
+
+        test('Validate method should correctly read invalid puzzle input strings', function () {
+            const puzzleStrings = invalidPuzzleStrings;
+            const test = puzzleStrings.every(str => (solver.validate(str).ok));
+            assert.isFalse(test, 'Validate function should return { ok: false, ... } when puzzle input string is invalid');
+        });
+
+        test('Validate method should return an error when puzzle input string length is greater than 81', function () {
+            let invalidPuzzleString = '1.5.2.8/44..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            let validationObj = solver.validate(invalidPuzzleString);
+            assert.property(validationObj, 'ok', 'validation object should contain ok');
+            assert.property(validationObj, 'error', 'validation object should contain error');
+            assert.isFalse(validationObj.ok, 'Validate function should return { ok: false, ... }');
+            assert.equal(validationObj.error, 'Expected puzzle to be 81 characters long');
+        });
+
+        test('Validate method should return an error when puzzle input string length is less than 81', function () {
+            let invalidPuzzleString = '1.5.2.8/..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            let validationObj = solver.validate(invalidPuzzleString);
+            assert.property(validationObj, 'ok', 'validation object should contain ok');
+            assert.property(validationObj, 'error', 'validation object should contain error');
+            assert.isFalse(validationObj.ok, 'Validate function should return { ok: false, ... }');
+            assert.equal(validationObj.error, 'Expected puzzle to be 81 characters long');
+        });
+
+        test('Validate method should return an error when puzzle input string contains invalid caracters', function () {
+            let invalidPuzzleString = '1.5.2.84%..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            let validationObj = solver.validate(invalidPuzzleString);
+            assert.property(validationObj, 'ok', 'validation object should contain ok');
+            assert.property(validationObj, 'error', 'validation object should contain error');
+            assert.isFalse(validationObj.ok, 'Validate function should return { ok: false, ... }');
+            assert.equal(validationObj.error, 'Invalid characters in puzzle');
+        });
+
+    });
 
 });
